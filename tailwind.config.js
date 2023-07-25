92b350516cbfec9c3dc76211d421df8bfd08038b
MD AAkil Raiyan Prantho <prantho98@gmail.com>
2023-07-26 01:32:27 +0600

    Footer Done

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+//   module.exports = {
+//     daisyui: {
+//       themes: ["cupcake", "dark", "cmyk"],
+//     },
+//   }
